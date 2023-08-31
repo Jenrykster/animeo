78f56c455082c4c6370ce9a25955797e7660ac2a
João Henrique <joaoenrique.san@gmail.com>
2023-08-31 16:15:41 -0300

    feat: add config page to the addon

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+const path = require("path");
 
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
-const addonInterface = require("./addon")
-serveHTTP(addonInterface, { port: process.env.PORT || 55985 })
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
+const addonInterface = require("./addon");
 
+serveHTTP(addonInterface, {
+  port: process.env.PORT || 55985,
+  customConfigPage: path.join(__dirname, "static", "config.html"),
+});
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
